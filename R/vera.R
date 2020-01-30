0c731b9a65b8b9b157c7a6ce65d7a78104f200b1
Jesus M. Castagnetto <jesus@castagnetto.com>
2020-01-30 17:27:04 -0500

    Fix typo in doc

--- R/vera.R
+++ R/vera.R
@@ -1,6 +1,6 @@
 #' The VERA computer acronyms list
 #'
-#' This package containes the complete list
+#' This package contains the complete list
 #' of computer acronyms from the "Virtual Entity of Relevant Acronyms" (VERA).
 #'
 #' The original source is at: https://www.ohei2.de/vera/vueber-e.html
